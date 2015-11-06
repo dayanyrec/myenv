0a7878a3896c58da56681c58beb9850cb4085240
Dayany Espindola <dayanyrec@gmail.com>
2015-11-06 00:06:58 -0200

    add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install g++ git vim inkscape gimp gimp-plugin-registry gimp-data-extras python-pip ipython ipython3
+
+sudo add-apt-repository ppa:staticfloat/juliareleases
+sudo add-apt-repository ppa:staticfloat/julia-deps
+sudo apt-get update
+sudo apt-get install julia
+
