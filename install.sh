0d5f278643a77796d5e893af8b7af3b666227179
Dayany Espindola <dayanyrec@gmail.com>
2015-11-18 14:35:58 -0200

    add curl and terminator

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install g++ git vim inkscape gimp gimp-plugin-registry gimp-data-extras python-pip ipython ipython3
+sudo apt-get install curl g++ git vim inkscape gimp gimp-plugin-registry gimp-data-extras python-pip ipython ipython3 terminator
 
 sudo add-apt-repository ppa:staticfloat/juliareleases
 sudo add-apt-repository ppa:staticfloat/julia-deps
